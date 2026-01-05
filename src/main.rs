87ef699c3214e1d11a34d57274fb8ce981c0d6bc
HassanAlphaSquad <hassanzahid.alphasquad@gmail.com>
2026-01-05 07:06:45 +0500

    Just testing muscle memory in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+    let a = 5;
+    let b = 20;
+    println!("{} + {} = {}", a, b, add(a, b))
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    return a + b;
 }
